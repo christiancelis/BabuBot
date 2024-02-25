e9b2cd747a116e1f61f21f5627fbfa2aaf6f17db
christiancelis <73555661+christiancelis@users.noreply.github.com>
2024-02-25 17:03:04 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,74 @@
+import { Client, IntentsBitField } from 'discord.js';
+
+import {config} from 'dotenv'
+
+config();
+
+
+const client = new Client({ intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+
+]});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+  
+client.on("messageCreate",(message)=>{
+
+    if(message.author.bot){
+        return
+    }
+
+    let Mensaje = message.content.toLowerCase()
+
+    if(Mensaje.includes("hello")){
+        message.reply(`hey bitch ${message.author.username}`)
+    }
+
+    if(Mensaje==="ping"){
+        message.reply("pong bitch")
+    }
+
+    if(Mensaje.includes("ja") || message.content.includes("ja")){
+        message.reply("No te rias perra")
+    }
+
+    if(Mensaje.includes("amito")){
+        message.reply("Amito bb")
+    }
+
+    if(Mensaje.includes("bubu")){
+        message.reply("bb estoy en clase amito")
+    }
+
+    if(Mensaje.includes("bitch")){
+        message.reply("Mas bitch seras tu")
+    }
+
+    if(Mensaje.includes("llegue")){
+        message.reply("nadie te estaba esperando.")
+    }
+
+    if(Mensaje.includes("pupu")){
+        message.reply("Cuidado tapa el baño Puerca!")
+    }
+
+    if(Mensaje.includes("pandita")){
+        message.reply("Todo esta en tu mente, cansona")
+    }
+    if(Mensaje.includes("perra")){
+        message.reply("Mas perra sera usted.")
+    }
+
+    if(Mensaje.includes("tesis")){
+        message.reply("Y es que no se piensa Graduar?")
+    }
+   
+})
+
+
+client.login(process.env.TOKEN);
