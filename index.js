04f6dc5f38bdd1ad7eda629619831f69dfd94c1a
ChristianCelis <73555661+christiancelis@users.noreply.github.com>
2024-02-25 22:05:37 -0500

    Update index.js a

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import { Client, IntentsBitField } from 'discord.js';
 import {http} from "http"
 
-http.createServer(function (req,res){
-    res.write("im alive");
-    res.end();
-}).listen(8080);
 
 import {config} from 'dotenv'
 
@@ -76,6 +72,12 @@ client.on("messageCreate",(message)=>{
     }
    
 })
+
+http.createServer(function (req,res){
+    res.write("im alive");
+    res.end();
+}).listen(8080);
+
 
 client.login(process.env.TOKEN);
 
